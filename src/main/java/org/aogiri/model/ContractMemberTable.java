bd4b105ff4a907ccf15f1b51d5c2d47eeba0373c
Lexi <lexithestrawhat@gmail.com>
2018-04-03 15:01:29 -0400

    Accidentally deleted normal contract creation last time.

--- src/main/java/org/aogiri/model/ContractMemberTable.java
+++ src/main/java/org/aogiri/model/ContractMemberTable.java
@@ -9,8 +9,36 @@ import java.util.Arrays;
 
 public class ContractMemberTable {
 
+
     /**
      * TODO: enforce that username is in account table
+     * @param conn
+     * @param id
+     * @param cycle_start
+     * @param cycle_bill
+     * @param username
+     * @return
+     */
+    public static boolean addContractMember(Connection conn,
+                                            int id,
+                                            String cycle_start,
+                                            float cycle_bill,
+                                            String username){
+
+        String query = String.format("INSERT INTO contract_member "
+                                    + "VALUES(%d,\'%s\',%2f,\'%s\');",
+                                    id, cycle_start, cycle_bill, username);
+        try {
+            Statement stmt = conn.createStatement();
+            stmt.execute(query);
+        } catch (SQLException e){
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * TODO: enforce that username is in non_contract_member table
      * ASSUMES users are already non-contract.
      * @param conn
      * @param cycle_start
