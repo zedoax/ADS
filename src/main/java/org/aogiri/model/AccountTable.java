3ecf1ef7b57aa761ed14656c55dae80385a42b2e
Lexi <lexithestrawhat@gmail.com>
2018-04-03 14:29:48 -0400

    Dates shouldn't be in quotes & got started on ContractMemberTable

---
+++ src/main/java/org/aogiri/model/AccountTable.java
@@ -0,0 +1,69 @@
+package org.aogiri.model;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+public class AccountTable {
+
+    /**
+     * Adds a single user to the database
+     * @param conn
+     * @param username
+     * @param password
+     * @param session_token
+     * @param first_name
+     * @param last_name
+     * @param address_number
+     * @param address_street
+     * @param address_city
+     * @param address_zipcode
+     * @return
+     */
+    public static boolean addAccount(Connection conn,
+                                     String username,
+                                     String password,
+                                     String session_token,
+                                     String first_name,
+                                     String last_name,
+                                     int address_number,
+                                     String address_street,
+                                     String address_city,
+                                     int address_zipcode){
+
+        String query = String.format("INSERT INTO account "
+                                    + "VALUES(\'%s\',\'%s\',\'%s\',\'%s\',\'%s\',%d,\'%s\',\'%s\',%d);",
+                                    username, password, session_token, first_name, last_name, address_number,
+                                    address_street, address_city, address_zipcode);
+        try {
+            Statement stmt = conn.createStatement();
+            stmt.execute(query);
+        } catch (SQLException e){
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * See example code PersonTable file.
+     * @param conn
+     * @param columns
+     * @param whereClauses
+     * @return
+     */
+    public static ResultSet queryAccountTable(Connection conn,
+                                              ArrayList<String> columns,
+                                              ArrayList<String> whereClauses){
+        return null;
+    }
+
+    // Loosely prototyped, feel free to change
+    public static boolean updateAccount(Connection conn,
+                                        String column,
+                                        String newValue,
+                                        ArrayList<String> whereClauses){
+        return false;
+    }
+}
--- src/main/java/org/aogiri/model/ContractMemberTable.java
+++ src/main/java/org/aogiri/model/ContractMemberTable.java
@@ -1,4 +1,72 @@
 package org.aogiri.model;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
 public class ContractMemberTable {
+
+    /**
+     * TODO: enforce that username is in account table
+     * @param conn
+     * @param id
+     * @param cycle_start
+     * @param cycle_bill
+     * @param username
+     * @return
+     */
+    public static boolean addContractMember(Connection conn,
+                                     int id,
+                                     String cycle_start,
+                                     float cycle_bill,
+                                     String username){
+
+        String query = String.format("INSERT INTO account "
+                                    + "VALUES(%d,\'%s\',%2f,\'%s\');",
+                                    id, cycle_start, cycle_bill, username);
+        try {
+            Statement stmt = conn.createStatement();
+            stmt.execute(query);
+        } catch (SQLException e){
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean deleteContractMember(Connection conn,
+                                               String username) {
+        String query = String.format("DELETE FROM test "
+                                    + "WHERE username=\'%s\';",
+                                    username);
+        try {
+            Statement stmt = conn.createStatement();
+            stmt.execute(query);
+        } catch (SQLException e){
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * See example code PersonTable file.
+     * @param conn
+     * @param columns
+     * @param whereClauses
+     * @return
+     */
+    public static ResultSet queryContractMemberTable(Connection conn,
+                                              ArrayList<String> columns,
+                                              ArrayList<String> whereClauses){
+        return null;
+    }
+
+    // Loosely prototyped, feel free to change
+    public static boolean updateAccount(Connection conn,
+                                        String column,
+                                        String newValue,
+                                        ArrayList<String> whereClauses){
+        return false;
+    }
 }
