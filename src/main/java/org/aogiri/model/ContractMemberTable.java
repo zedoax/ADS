7dddc06dcb053b48ff6e33c4e3bd862636788cb8
Lexi <lexithestrawhat@gmail.com>
2018-04-03 14:58:59 -0400

    Added non_contract_member stuff and added function to ContractMemberTable to change from non-contract to contract.

--- src/main/java/org/aogiri/model/ContractMemberTable.java
+++ src/main/java/org/aogiri/model/ContractMemberTable.java
@@ -5,30 +5,32 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class ContractMemberTable {
 
     /**
      * TODO: enforce that username is in account table
+     * ASSUMES users are already non-contract.
      * @param conn
-     * @param id
      * @param cycle_start
      * @param cycle_bill
      * @param username
      * @return
      */
-    public static boolean addContractMember(Connection conn,
-                                     int id,
+    public static boolean changeToContractMember(Connection conn,
                                      String cycle_start,
                                      float cycle_bill,
                                      String username){
-
         String query = String.format("INSERT INTO contract_member "
-                                    + "VALUES(%d,\'%s\',%2f,\'%s\');",
-                                    id, cycle_start, cycle_bill, username);
+                                    + "SELECT   non_contract_member.member_id, \'%s\' , %2f, \'%s\' "
+                                    + "FROM     non_contract_member "
+                                    + "WHERE    non_contract_member.username=\'s\';",
+                                    cycle_start, cycle_bill, username, username);
         try {
             Statement stmt = conn.createStatement();
             stmt.execute(query);
+            if(!NonContractMemberTable.deleteContractMember(conn, username)) return false;
         } catch (SQLException e){
             return false;
         }
--- src/main/java/org/aogiri/model/NonContractMemberTable.java
+++ src/main/java/org/aogiri/model/NonContractMemberTable.java
@@ -1,4 +1,61 @@
 package org.aogiri.model;
 
-public class NonContractMemberTable {
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+public class NonContractMemberTable{
+
+    public static boolean addNonContractMember(Connection conn,
+                                               int memberID,
+                                               String username){
+
+        String query = String.format("INSERT INTO non_contract_member "
+                                    + "VALUES(%d,\'%s\');",
+                                    memberID, username);
+        try {
+            Statement stmt = conn.createStatement();
+            stmt.execute(query);
+        } catch (SQLException e){
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean deleteContractMember(Connection conn,
+                                               String username) {
+        String query = String.format("DELETE FROM test "
+                        + "WHERE username=\'%s\';",
+                username);
+        try {
+            Statement stmt = conn.createStatement();
+            stmt.execute(query);
+        } catch (SQLException e){
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * See example code PersonTable file.
+     * @param conn
+     * @param columns
+     * @param whereClauses
+     * @return
+     */
+    public static ResultSet queryContractMemberTable(Connection conn,
+                                                     ArrayList<String> columns,
+                                                     ArrayList<String> whereClauses){
+        return null;
+    }
+
+    // Loosely prototyped, feel free to change
+    public static boolean updateAccount(Connection conn,
+                                        String column,
+                                        String newValue,
+                                        ArrayList<String> whereClauses){
+        return false;
+    }
 }
